d2d06e8a267203e1fb1185516c013f2e63537a1e
Stefan <kondinskis@gmail.com>
2017-04-28 15:50:54 +0200

    Merge branch

--- src/main/java/com/fict/controllers/TestController.java
+++ src/main/java/com/fict/controllers/TestController.java
@@ -3,7 +3,9 @@ package com.fict.controllers;
 import com.fict.entities.TestEntity;
 import com.fict.services.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,11 +18,11 @@ public class TestController {
     private TestService service;
 
 
-    @RequestMapping("/")
-    public String sayHello(){
-        TestEntity testEntity = service.findTestEntityById(1L);
-        System.out.println(testEntity.getName());
-        return testEntity.getName();
+    @RequestMapping("/{id}")
+    public String sayHello(@PathVariable Long id){
+        TestEntity testEntity = service.findTestEntityById(id);
+        System.out.println(testEntity.getFirstname());
+        return testEntity.getFirstname();
     }
 
 }
--- src/main/java/com/fict/entities/TestEntity.java
+++ src/main/java/com/fict/entities/TestEntity.java
@@ -6,13 +6,20 @@ import javax.persistence.*;
  * Created by stevo on 4/27/17.
  */
 @Entity
-@Table(name = "person")
+@Table(name = "customer")
 public class TestEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String embg;
+    private String address;
+    private String email;
+    private String transaction_number;
+    private float balance;
 
     public Long getId() {
         return id;
@@ -22,11 +29,67 @@ public class TestEntity {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmbg() {
+        return embg;
+    }
+
+    public void setEmbg(String embg) {
+        this.embg = embg;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTransaction_number() {
+        return transaction_number;
+    }
+
+    public void setTransaction_number(String transaction_number) {
+        this.transaction_number = transaction_number;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
     }
 }
